97fbb9e1a854c1e5079102974b7daa309e2518ea
KB-786 <karanbansal0786@gmail.com>
2020-12-01 19:33:21 +0530

    database connected

--- src/Home/customerForm.java
+++ src/Home/customerForm.java
@@ -5,11 +5,12 @@ import java.sql.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+//import Test.DBUtil;
 
 public class customerForm extends HttpServlet{
 
 	 protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-	
+			 
 		 PrintWriter out=res.getWriter();
 		 
 		 String uname=req.getParameter("uname");
@@ -21,6 +22,31 @@ public class customerForm extends HttpServlet{
 		 
 		 out.println(uname+" "+radio+" "+model+" "+city+" "+phone+" "+date);
 		 
-		 
+		 Connection con=DBUtil.getDBConnection();
+		  
+		  try {  
+		  PreparedStatement ps=con.prepareStatement("insert into fcs.customer values(?, ?, ?, ?, ?, ?)");
+//		  PreparedStatement ss=con.prepareStatement("select * from fcs.customer");
+		  ps.setString(1, uname);
+		  ps.setString(2, radio);
+		  ps.setString(3, model);
+		  ps.setString(4, city);
+		  ps.setString(5, phone);
+		  ps.setString(6, date);
+		  ResultSet rs=ps.executeQuery();
+//		  ResultSet rs=ss.executeQuery();
+//		  if(rs.next()) {
+//			  String str = rs.getString("uname");
+//			  out.println(str);
+//
+//		  }
+		  
+
+		  }
+		  catch(SQLException e)
+		  {
+			  System.out.println("Data not found");
+			  e.printStackTrace();
+		  }
 	 }
 }
